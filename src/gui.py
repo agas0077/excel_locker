28ec5c20fdf778e651d6446dc35476819f724e85
XeoPlay <xeoplay123456@gmail.com>
2020-10-09 12:05:15 +0300

    added localization

--- src/gui.py
+++ src/gui.py
@@ -5,9 +5,39 @@ from PasswordSetter import *
 from EntryField import *
 from Lables import *
 
-# Создаем окно с необходимыми параметрами
+with open("config.txt", "r") as f:
+    if f.readline() == "RU":
+        from RU import *
+    else:
+        from ENG import *
+
+# Создаем окно
 window = Tk()
-window.title('Установка паролей на редактирование файлов Excel')
+
+def restart_program():
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+
+# Создаём функции для переключения языков
+def set_lang_ru():
+    with open('config.txt', 'w') as f:
+        f.write('RU')
+    restart_program()
+
+def set_lang_eng():
+    with open('config.txt', 'w') as f:
+        f.write('ENG')
+    restart_program()
+
+# Создаём кнопки переключения языков
+lang_ru_btn = Button(window, text="Русский", command=set_lang_ru)
+lang_eng_btn = Button(window, text="English", command=set_lang_eng)
+
+lang_ru_btn.pack(side=TOP, anchor=NW, padx=5, pady=5)
+lang_eng_btn.pack(side=TOP, anchor=NW, padx=5, pady=5)
+
+# Конфигурируем окно
+window.title(TITLE)
 window.geometry("500x300")
 window.resizable(width=False, height=False)
 window.configure(background='#2e8b57')
@@ -15,7 +45,7 @@ window.configure(background='#2e8b57')
 # Создаем экемпляр класса диалогового окна
 dialog = Dialog()
 # Создаем экземпляр класса поля ввода
-passField = EntryField(window, "Пароль")
+passField = EntryField(window, ENTRY_TEXT)
 # Создаем экземпляр класса установщика паролей
 ps = PasswordSetter()
 # Создаем экземпляр лейбла
@@ -34,13 +64,13 @@ def runLocking(mypassword=None, button=None):
 
     # Проверяем выбран ли файлы
     if len(tup) == 0:
-        return messagebox.showerror("Ошибка", "Необходимо выбрать файлы")
+        return messagebox.showerror(ERROR, ERROR1)
     # Проверяем введен ли пароль
     if button: 
         if len(password) == 0 or password == passField.placeholder:
-            return messagebox.showerror("Ошибка", "Необходимо ввести пароль")
+            return messagebox.showerror(ERROR, ERROR2)
         if len(password) > 15:
-            return messagebox.showerror("Ошибка", "Пароль должен состоять из максимум 15 символов")
+            return messagebox.showerror(ERROR, ERROR3)
 
     i = 0
     errors = 0
@@ -56,10 +86,10 @@ def runLocking(mypassword=None, button=None):
         window.update()
         res = ps.setPassword(path, name, password)
         if res["err"]:
-            message = f"Ошибка: {res['err']}"
+            message = f"{ERROR}: {res['err']}"
             errors += 1
         else:
-            message = f"Пароль установлен для {dialog.fileNames[i]}"
+            message = f"{PASSWORD_INSTALLED_MSG} {dialog.fileNames[i]}"
         # Добавляем сообщение об успешном выполнении
         lb.addMessageToList(f"{message}", 6)
         lb.destroyLables()
@@ -72,12 +102,12 @@ def runLocking(mypassword=None, button=None):
 
     # Удаляем сообщения об успешном выполнении и выводим сообщение об успешном завершении процесса
     lb.destroyLables()
-    lb.addMessageToList(f"ВСЕ ФАЙЛЫ БЫЛИ ОБРАБОТАНЫ! Ошибок: {errors}", 6)
+    lb.addMessageToList(f"{SUCCESS}{errors}", 6)
     lb.createLables(background='#2e8b57')
         
 
 # Инициализация кнопки выбора файлов
-chooseFileBtn = Button(window, text="Выбрать файлы", command=dialog.callDialog, width=25, height=1)
+chooseFileBtn = Button(window, text=CHOOSE_FILES_BTN, command=dialog.callDialog, width=25, height=1)
 chooseFileBtn.pack(side=TOP, padx=5, pady=5)
 
 # Инициализация поля ввода пароля
@@ -85,7 +115,7 @@ passField.configure(width=30, justify=CENTER)
 passField.pack(side=TOP, padx=5, pady=5)
 
 # Инициализация кнопки запуска
-showFileBtn = Button(window, text="Запустить обработку файлов", command=runLocking, width=25, height=1)
+showFileBtn = Button(window, text=START_PROCESSING_BTN, command=runLocking, width=25, height=1)
 showFileBtn.pack(padx=5, pady=5)
 
 # Инициализация кнопки запуска со стандартным паролем
